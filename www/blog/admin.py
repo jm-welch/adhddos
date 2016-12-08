116713d93355d4c3719f265cb61cbd1db6029cba
Jeremy Welch <jm-welch@users.noreply.github.com>
2016-12-08 16:59:40 -0600

    Initial commit (fixes #15)

---
+++ www/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+from django.db.models.functions import Lower
+from django.utils.text import slugify
+import itertools
+
+# Register your models here.
+
+# @admin.register(<model>)
+# class <Model>Admin(admin.ModelAdmin):
+#     pass
