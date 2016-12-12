6055804a60a81c2963e4135cb09d3cda3ab04de6
Jeremy Welch <jm-welch@users.noreply.github.com>
2016-12-12 14:13:34 -0600

    Updated wgsi.py for hosting (#3)

--- www/www/wsgi.py
+++ www/www/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "www.settings")
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
