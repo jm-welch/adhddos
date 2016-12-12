a76edd93189b9b1485e79bb582a6436f0e9e0636
Jeremy Welch <jm-welch@users.noreply.github.com>
2016-12-12 14:46:59 -0600

    Unique slug (#18)

--- www/blog/models.py
+++ www/blog/models.py
@@ -1,5 +1,7 @@
+import itertools
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,14 +10,32 @@ class Post(models.Model):
     tags = models.CharField(max_length=200,
                             blank=True, null=True,
                             help_text="Comma-separated list")
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True,
+                            help_text="Auto-generated from title. Must be unique. Clear field and save to generate new slug.")
     created_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
     views = models.IntegerField(default=0)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.publish_date = timezone.now()
         self.save()
+
+    def make_slug(self):
+        """ Set the slug to enforce uniqueness """
+        slug = orig = slugify(self.title)
+
+        for x in itertools.count(1):
+            if not Post.objects.filter(slug=slug).exists():
+                break
+            slug = '{s}-{n}'.format(s=orig, n=x)
+
+        return slug
+
+    def save(self, *args, **kwargs):
+        # Override the default .save() method to enforce a unique slug
+        if not self.slug:
+            self.slug = self.make_slug()
+        super(Post, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
