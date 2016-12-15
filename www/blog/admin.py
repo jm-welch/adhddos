d5f5a713b062943faa450e7d0d9ce30ec8f3d575
Jeremy Welch <jm-welch@users.noreply.github.com>
2016-12-15 15:29:24 -0600

    Updated modeladmin (fixes #16, #17)

--- www/blog/admin.py
+++ www/blog/admin.py
@@ -4,9 +4,24 @@ from django.db.models.functions import Lower
 from django.utils.text import slugify
 import itertools
 
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    #TODO: Show more columns id:16
-    #TODO: "Publish" option for post id:17
-    pass
+    def PublishPost(self, request, queryset):
+        for post in queryset:
+            post.publish()
+    PublishPost.short_description = "Publish post now"
+
+    def UnPublishPost(self, request, queryset):
+        for post in queryset:
+            post.publish_date = ''
+    UnPublishPost.short_description = "Unpublish post"
+
+    list_display = ['title', 'publish_date', 'tags']
+    #TODO: Find a better way to make the publish function quickly editable from the admin
+    # list_editable = ['publish_date']
+    search_fields = ['title', 'body', 'tags']
+    actions = [PublishPost, UnPublishPost]
+
+    #TODO: Find a way to filter by tags in taglist
+
+    date_hierarchy = 'created_date'
